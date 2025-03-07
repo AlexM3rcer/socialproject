574dae867e4f6af0b73c21549f984676db99311e
Alexey Melnikov <marvel12612@gmail.com>
2025-03-07 12:35:51 +0300

    modified:   src/backend/server.js 	modified:   src/frontend/Map.js 	new file:   src/frontend/images/info.png 	new file:   src/frontend/marker-icon-2x.png

--- src/frontend/Map.js
+++ src/frontend/Map.js
@@ -4,58 +4,62 @@ import HeatmapOverlay from 'heatmap.js/plugins/leaflet-heatmap';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet.markercluster/dist/MarkerCluster.css';
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
-import MarkerClusterGroup from 'leaflet.markercluster';
+import 'leaflet.markercluster';
 
 const Map = () => {
     const mapRef = useRef(null);
-    const [rad, setRad] = useState(0.1); // Начальное значение радиуса
-    const [opac, setOpac] = useState(0.8); // Начальное значение прозрачности
-    const [mapCenter, setMapCenter] = useState([55.76, 37.64]); // Центр карты
-    const [mapZoom, setMapZoom] = useState(10); // Уровень масштабирования
+    const [rad, setRad] = useState(0.1);
+    const [opac, setOpac] = useState(0.8);
+    const [mapCenter, setMapCenter] = useState([55.76, 37.64]);
+    const [mapZoom, setMapZoom] = useState(10);
 
-    // Загрузка данных с бэкенда
     const fetchData = async () => {
         try {
             const response = await fetch('http://localhost:5000/api/heritage');
-            const data = await response.json(); // data уже является объектом
-            return data; // Возвращаем данные как есть
+            const data = await response.json();
+            return data;
         } catch (error) {
             console.error('Ошибка при загрузке данных:', error);
             return [];
         }
     };
 
+    const customIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        shadowSize: [41, 41]
+    });
+
+
     useEffect(() => {
-        // Добавление слоя с тайлами OpenStreetMap
         var baseLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '© OpenStreetMap contributors',
         });
 
-        // Конфигурация тепловой карты
         var heatmapLayer = new HeatmapOverlay({
-            radius: rad, // Используем значение из состояния
-            maxOpacity: opac, // Используем значение из состояния
-            scaleRadius: true, // Масштабирование радиуса
-            useLocalExtrema: true, // Использование локальных экстремумов
-            latField: 'lat', // Поле для широты
-            lngField: 'lng', // Поле для долготы
-            valueField: 'value' // Поле для значения
+            radius: rad,
+            maxOpacity: opac,
+            scaleRadius: true,
+            useLocalExtrema: true,
+            latField: 'lat',
+            lngField: 'lng',
+            valueField: 'value'
         });
 
-        // Инициализация карты
         const map = new L.Map(mapRef.current, {
-            center: mapCenter, 
-            zoom: mapZoom,     
+            center: mapCenter,
+            zoom: mapZoom,
             layers: [baseLayer, heatmapLayer]
         });
 
-        // Установка ограничений на границы карты
-        const southWest = L.latLng(-90, -180); 
-        const northEast = L.latLng(90, 180); 
+        const southWest = L.latLng(-90, -180);
+        const northEast = L.latLng(90, 180);
         const bounds = L.latLngBounds(southWest, northEast);
         map.setMaxBounds(bounds);
 
-        // Сохранение текущего центра и уровня масштабирования
         map.on('moveend', () => {
             setMapCenter(map.getCenter());
         });
@@ -64,25 +68,34 @@ const Map = () => {
             setMapZoom(map.getZoom());
         });
 
-        // Загрузка данных и установка их в тепловую карту
+        // Создаем группу кластеризации
+        const markers = L.markerClusterGroup();
+
         fetchData().then(points => {
             heatmapLayer.setData({
-                max: 1, // Максимальное значение
-                data: points, // Устанавливаем данные
+                max: 1,
+                data: points,
             });
+
+            points.forEach(point => {
+                const marker = L.marker([point.lat, point.lng], {icon: customIcon});
+                marker.bindPopup(`<b>${point.name}</b><br>${point.address}`);
+                markers.addLayer(marker);
+            });
+
+            map.addLayer(markers);
         });
 
-        // Очистка при размонтировании компонента
         return () => {
             map.remove();
         };
-    }, [rad, opac]); // Зависимости: rad и opac
+    }, [rad, opac]);
 
     return (
         <div className="placeholder">
             <form id='mapSettings'>
                 <div>
-                    <label className='mapLabel'>Размер свечения:</label>
+                    <label className='mapLabel'>Масштаб теплового слоя:</label>
                     <input className='mapInput' type="number" value={rad} onChange={(e) => setRad(parseFloat(e.target.value))} required />
                 </div>
                 <div>
